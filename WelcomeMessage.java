0edffb0a0565c7a40f42e633a88c61c3b5863ea7
sbmtechPro <martinyeboah@sbmtechpro.com>
2024-06-16 11:49:05 -0500

    Update WelcomeMessage.java

--- WelcomeMessage.java
+++ WelcomeMessage.java
@@ -1,5 +1,5 @@
 public class WelcomeMessage {
     public static void main(String[] args) {
-        System.out.println("Welcome to sbmtechPro training class.");
+        System.out.println("This is working fine .Thank you.");
     }
 }
